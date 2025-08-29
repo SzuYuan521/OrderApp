319dae5fe9131d12c0c17fa53245d5ed9cf88f01
Bear <SzuYuanChen.ssey>
2025-08-29 17:50:20 +0800

    [*] 補 import watch

--- src/components/NotifySettings.vue
+++ src/components/NotifySettings.vue
@@ -15,7 +15,7 @@
 
 
 <script setup>
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
 import { store } from '../composables/useStore';
 import { sendDiscordNotification } from '../composables/useNotify';
 
