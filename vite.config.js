dcb45ff1f41f363665adb47f1aa9b32d06a40810
Bear <SzuYuanChen.ssey>
2025-08-29 17:10:48 +0800

    [!] 更新 GithubPage 環境設定

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "./" // 讓 GitHub Pages 能正確載入
+  base: process.env.BASE_URL || '/' // 讓 GitHub Pages 能正確載入
 });
